8c4d58c77829e43848f342458111c9cbee30e104
Your Name <you@example.com>
2018-12-31 15:44:07 +0000

    2a

--- 2a.py
+++ 2a.py
@@ -1,10 +1,25 @@
-INPUT = "1a-input.txt"
+from collections import defaultdict
 
-current_frequency = 0
+INPUT = "2a-input.txt"
+
+twos_and_threes = defaultdict(int)
+
+def do_boxID_freqs(boxID):
+    global twos_and_threes
+    frequency = defaultdict(int)
+    for letter in boxID:
+        frequency[letter] += 1
+
+    if 2 in frequency.values():
+        twos_and_threes['2'] += 1
+
+    if 3 in frequency.values():
+        twos_and_threes['3'] += 1
+
 
 with open(INPUT, 'r') as infile:
-    for line in infile:
-        change = int(line)
-        current_frequency += change
-        print(change, current_frequency)
+    for boxID in infile:
+        do_boxID_freqs(boxID)
 
+print("Twos: {}, Threes: {}, checksum: {}".format(twos_and_threes['2'],
+       twos_and_threes['3'], twos_and_threes['2'] * twos_and_threes['3']))
